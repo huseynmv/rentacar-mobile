675d4fab90f0e3c331c5225e6e0f142d52a10c76
huseynmv <huseynmv@yahoo.com>
2023-03-18 18:57:09 +0400

    onboarding

--- App.tsx
+++ App.tsx
@@ -1,21 +1,49 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {StyleSheet, Text, View, Image} from 'react-native';
+import React, {useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Provider, useSelector} from 'react-redux';
-
+import Onboarding from 'react-native-onboarding-swiper';
 import Tab from './src/navigation';
 import {store} from './src/features';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 const App = () => {
   // console.log(AsyncStorage.getItem('favorites'));
+  const [isOnboarding, setisOnboarding] = useState<boolean>(true);
 
   return (
     <>
-      <Provider store={store}>
-        <NavigationContainer>
-          <Tab />
-        </NavigationContainer>
-      </Provider>
+      {isOnboarding ? (
+        <Onboarding
+          onDone={() => setisOnboarding(false)}
+          onSkip={() => setisOnboarding(false)}
+          pages={[
+            {
+              backgroundColor: 'red',
+              title: 'salam',
+              subtitle: 'salam subtiitle',
+              image: <Image source={require('./src/assets/car.png')} />,
+            },
+            {
+              backgroundColor: 'red',
+              title: 'salam2',
+              subtitle: 'salam subtiitle',
+              image: <Image source={require('./src/assets/car.png')} />,
+            },
+            {
+              backgroundColor: 'red',
+              title: 'salam3',
+              subtitle: 'salam subtiitle',
+              image: <Image source={require('./src/assets/car.png')} />,
+            },
+          ]}
+        />
+      ) : (
+        <Provider store={store}>
+          <NavigationContainer>
+            <Tab />
+          </NavigationContainer>
+        </Provider>
+      )}
     </>
   );
 };
