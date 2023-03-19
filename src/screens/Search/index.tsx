7686d020dd9b71f064ebcd9168cc7d0f0d8c66f2
huseynmv <huseynmv@yahoo.com>
2023-03-19 14:26:57 +0400

    search input

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,11 +1,57 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+} from 'react-native';
+import React, {useState} from 'react';
 
 const Index = () => {
+  const [searchText, setsearchText] = useState<string>('');
+  const [data, setdata] = useState<any>();
+  const [isLoading, setisLoading] = useState<boolean>(true);
+
+  const getData = () => {
+    fetch(`https://rent-a-car-api.onrender.com/api/car/getobj/${searchText}`)
+      .then(res => res.json())
+      .then(datas => {
+        setdata(datas);
+        setisLoading(false);
+      });
+  };
+
+  const renderitem = ({item}: any) => {
+    return (
+      <>
+        <Text>{item.name}</Text>
+      </>
+    );
+  };
+
   return (
-    <View>
-      <Text>Search Screen</Text>
-    </View>
+    <>
+      {isLoading ? (
+        <View>
+          <TextInput
+            onChangeText={setsearchText}
+            style={{
+              height: 50,
+              width: 300,
+              margin: 12,
+              borderWidth: 1,
+              padding: 10,
+            }}
+          />
+          <Button title="search" onPress={() => getData()}></Button>
+        </View>
+      ) : (
+        <View>
+          <FlatList data={data} renderItem={renderitem} />
+        </View>
+      )}
+    </>
   );
 };
 
