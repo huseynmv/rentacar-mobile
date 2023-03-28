542f1f9a65d84d4ccc47cfe69655da267051c81c
huseynmv <huseynmv@yahoo.com>
2023-03-28 19:33:15 +0400

    login page style

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -8,11 +8,14 @@ import {
 } from 'react-native';
 import React, {useState} from 'react';
 import axios from 'axios';
+import {MaterialIndicator} from 'react-native-indicators';
 
 const Login = ({navigation}: any) => {
   const [Useremail, setUseremail] = useState('');
+  const [loading, setloading] = useState(false);
 
   const confirmLogin = () => {
+    setloading(true);
     console.log(Useremail);
 
     axios
@@ -22,35 +25,80 @@ const Login = ({navigation}: any) => {
       .then(data => {
         if (data.data.statusCode == 200) {
           navigation.navigate('MyTabs');
+          setloading(false);
         }
       });
   };
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <View style={{flex: 1, justifyContent: 'center'}}>
       <View
         style={{
           justifyContent: 'center',
           alignItems: 'center',
-          // marginVertical: 50,
         }}>
-        <Text style={{fontSize: 24, fontWeight: '700'}}>
-          Welcome to our app
-        </Text>
         <Text style={{fontSize: 20, fontWeight: '500'}}>
           Login to your account
         </Text>
-      </View>
-      <View>
         <TextInput
-          style={{height: 50, width: 200, borderRadius: 12, borderWidth: 1}}
-          placeholder="Email"
           onChangeText={setUseremail}
-        />
-        <Button title="Login" onPress={() => confirmLogin()} />
+          placeholder="Your email adress"
+          style={{
+            width: '70%',
+            height: 45,
+            borderWidth: 2,
+
+            // marginLeft: 60,
+            // marginRight: 60,
+            marginTop: 10,
+            borderRadius: 12,
+            borderColor: '#94A1B2',
+          }}></TextInput>
+        {loading ? (
+          <TouchableOpacity
+            style={{
+              width: '70%',
+              height: 45,
+              borderRadius: 10,
+              opacity: 0.7,
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: '#2CB67D',
+              marginHorizontal: 20,
+              marginTop: 10,
+            }}>
+            <MaterialIndicator />
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity
+            onPress={() => confirmLogin()}
+            style={{
+              width: '70%',
+              height: 45,
+              borderRadius: 10,
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: '#2CB67D',
+              marginHorizontal: 20,
+              marginTop: 10,
+            }}>
+            <Text style={{color: '#FFF', fontSize: 20}}>Login</Text>
+          </TouchableOpacity>
+        )}
+        <View
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: 5,
+            flexDirection: 'row',
+          }}>
+          <Text style={{fontSize: 16}}>Dont have an account?</Text>
+          <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+            <Text style={{color: '#2CB67D', marginLeft: 5, fontSize: 16}}>
+              Register
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      <TouchableOpacity onPress={() => navigation.navigate('Register')}>
-        <Text>Register</Text>
-      </TouchableOpacity>
     </View>
   );
 };
