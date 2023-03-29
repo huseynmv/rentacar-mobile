a2c99de9042f19f0377d7007da43215cb15310ed
huseynmv <huseynmv@yahoo.com>
2023-03-29 21:09:20 +0400

    search text style

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -5,9 +5,12 @@ import {
   TextInput,
   Button,
   FlatList,
+  TouchableOpacity,
 } from 'react-native';
 import React, {useState} from 'react';
 import CarCard from '../../components/CarCard';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 
 const Index = ({navigation}: any) => {
   const [searchText, setsearchText] = useState<string>('');
@@ -15,7 +18,7 @@ const Index = ({navigation}: any) => {
   const [isLoading, setisLoading] = useState<boolean>(true);
 
   const getData = () => {
-    fetch(`https://rent-a-car-api.onrender.com/api/car/getobj/${searchText}`)
+    fetch(`https://rent-car-api.onrender.com/api/car/getobj/${searchText}`)
       .then(res => res.json())
       .then(datas => {
         setdata(datas);
@@ -34,18 +37,28 @@ const Index = ({navigation}: any) => {
   return (
     <>
       {isLoading ? (
-        <View>
+        <View
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'row',
+          }}>
           <TextInput
             onChangeText={setsearchText}
             style={{
               height: 50,
-              width: 300,
+              width: '90%',
+              borderRadius: 30,
               margin: 12,
               borderWidth: 1,
               padding: 10,
             }}
           />
-          <Button title="search" onPress={() => getData()}></Button>
+          <View>
+            <TouchableOpacity onPress={() => getData()}>
+              <FontAwesomeIcon icon={faSearch} size={24} />
+            </TouchableOpacity>
+          </View>
         </View>
       ) : (
         <View>
